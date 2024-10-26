f42d6268d48123eb9b7973678bb86ed0b2fd29ab
Павел <pavchekin@mail.ru>
2024-10-27 02:23:52 +0300

    1213

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const { Linter } = require('eslint');
+
+/** @type {Linter.Config} */
+const config = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+};
+
+module.exports = config;
